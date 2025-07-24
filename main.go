8ec6d40177be077765e07d5f91c19ba6a55b8967
Shercosta <shercostagp@gmail.com>
2025-07-24 17:29:13 +0700

    initialize linkmatch

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	findEnv()
+	port := os.Getenv("PORT")
+
+	router := gin.Default()
+
+	router.Use(cors.Default())
+
+	fmt.Println("Starting server on port", port)
+	router.Run(":" + port)
+}
+
+func findEnv() { // find the .env file on the current root project ('../') and load it
+	possibleEnv := []string{
+		"./.env.example",
+		"./.env.production",
+		"./.env.development",
+		"./.env.local",
+		"./.env",
+	}
+
+	for _, env := range possibleEnv {
+		if _, err := os.Stat(env); err == nil {
+			fmt.Println("Loading", env)
+			_ = godotenv.Load(env)
+			return
+		}
+	}
+}
